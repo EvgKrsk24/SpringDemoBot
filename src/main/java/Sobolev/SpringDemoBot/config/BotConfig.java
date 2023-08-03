40690c5c8bf2a7400412c4bc2b8ebbb9616dfa48
EvgKrsk24 <chiaevgkrsk@gmail.com>
2023-08-03 14:01:23 +0700

    The bot executes commands

---
+++ src/main/java/Sobolev/SpringDemoBot/config/BotConfig.java
@@ -0,0 +1,20 @@
+package Sobolev.SpringDemoBot.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import lombok.Data;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@Data
+@PropertySource("application.properties")
+public class BotConfig {
+    @Value("${bot.name}")
+    String botname;
+
+    @Value("${bot.token}")
+    String token;
+
+
+}
